18c9f1c81e500bffa697a5e398fee0fed898c44f
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-08 12:58:34 +0900

    틱을 사용한 cpu 성능 확인 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csloop
@@ -18,11 +19,6 @@ namespace csloop
             }
             Console.WriteLine(count + "만큼 반복함");
 
-            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
-            foreach (string item in array)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
